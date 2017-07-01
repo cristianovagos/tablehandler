864ba1e7f61283c715a4cc4c5284b132042a7920
Cristiano Vagos <cvagos@ubiwhere.pt>
2017-07-01 20:31:49 +0100

    Semantic check finished, added one more working example

--- THSemanticCheck.java
+++ THSemanticCheck.java
@@ -4,7 +4,7 @@ public class THSemanticCheck extends TableHandlerBaseVisitor<Boolean> {
     
     private ParseTreeProperty<TYPE> typeNode = new ParseTreeProperty<>();
 
-    public enum TYPE {NUMBER, BOOLEAN, STRING, ID, DOUBLE};
+    public enum TYPE {NUMBER, BOOLEAN, STRING, DOUBLE};
 
     @Override public Boolean visitPrintExpr(TableHandlerParser.PrintExprContext ctx) {
         boolean res = visit(ctx.expr());
@@ -13,7 +13,22 @@ public class THSemanticCheck extends TableHandlerBaseVisitor<Boolean> {
             ErrorHandling.printError(ctx, "type expression " + ctx.expr().getText() + " is not a number!");
             res = false;
         }
+        else if(res && typeNode.get(ctx.expr()) != TYPE.BOOLEAN)
+        {
+            ErrorHandling.printError(ctx, "type expression " + ctx.expr().getText() + " is not a boolean!");
+            res = false;
+        }
+        else if(res && typeNode.get(ctx.expr()) != TYPE.STRING)
+        {
+            ErrorHandling.printError(ctx, "type expression " + ctx.expr().getText() + " is not a string!");
+            res = false;
+        }
         return res;
+    }
+
+    @Override public Boolean visitBoolExpr(TableHandlerParser.BoolExprContext ctx) { 
+        typeNode.put(ctx, TYPE.BOOLEAN);
+        return true;
     }
 
     @Override public Boolean visitIfStatement(TableHandlerParser.IfStatementContext ctx) { 
@@ -58,6 +73,7 @@ public class THSemanticCheck extends TableHandlerBaseVisitor<Boolean> {
     }
 
     @Override public Boolean visitPar(TableHandlerParser.ParContext ctx) {
+        typeNode.put(ctx, TYPE.NUMBER);
         boolean res = visit(ctx.numExpr());
         if (res)
             typeNode.put(ctx, typeNode.get(ctx.numExpr()));
@@ -70,12 +86,12 @@ public class THSemanticCheck extends TableHandlerBaseVisitor<Boolean> {
     }
 
     @Override public Boolean visitDouble(TableHandlerParser.DoubleContext ctx) { 
-        typeNode.put(ctx, TYPE.DOUBLE);
+        typeNode.put(ctx, TYPE.NUMBER);
         return true;
     }
 
     @Override public Boolean visitVar(TableHandlerParser.VarContext ctx) {
-        typeNode.put(ctx, TYPE.ID);
+        typeNode.put(ctx, TYPE.NUMBER);
         return true;
     }
 
@@ -97,7 +113,7 @@ public class THSemanticCheck extends TableHandlerBaseVisitor<Boolean> {
 
     @Override public Boolean visitNewTable(TableHandlerParser.NewTableContext ctx) {
         boolean res = visit(ctx.file());
-        if(res && typeNode.get(ctx.file()) != TYPE.ID)
+        if(res && typeNode.get(ctx.file()) != TYPE.STRING)
         {
             ErrorHandling.printError(ctx, " type expression " + ctx.file().getText() + " is not defined!");
             res = false;
@@ -111,8 +127,31 @@ public class THSemanticCheck extends TableHandlerBaseVisitor<Boolean> {
     }
 
     @Override public Boolean visitFile(TableHandlerParser.FileContext ctx) { 
-        typeNode.put(ctx, TYPE.ID);
+        typeNode.put(ctx, TYPE.STRING);
         return true;
     }
 
+    @Override public Boolean visitSetValue(TableHandlerParser.SetValueContext ctx) {
+        boolean res = visit(ctx.expr());
+        if(res && typeNode.get(ctx.expr()) != TYPE.STRING)
+        {
+            ErrorHandling.printError(ctx, " type expression " + ctx.expr().getText() + " is not defined!");
+            res = false;
+        }
+        return res;
+    }
+
+    @Override public Boolean visitExport(TableHandlerParser.ExportContext ctx) { 
+        boolean res = visit(ctx.file());
+        if(res && typeNode.get(ctx.file()) != TYPE.STRING)
+        {
+            ErrorHandling.printError(ctx, " type expression " + ctx.file().getText() + " is not defined!");
+            res = false;
+        }
+        return res;
+    }
+
+    @Override public Boolean visitConditionBlock(TableHandlerParser.ConditionBlockContext ctx) {
+        return false;
+    }
 }
